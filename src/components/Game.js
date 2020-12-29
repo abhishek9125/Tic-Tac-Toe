ce1ca17052614a562eeb98b4b7490ad83bcdc5b2
abhishek9125 <61241956+abhishek9125@users.noreply.github.com>
2020-12-29 22:25:28 +0530

    App Completed

--- src/components/Game.js
+++ src/components/Game.js
@@ -19,6 +19,12 @@ class Game extends React.Component{
         const current = history[history.length - 1];    // Last object of History Array
         const squares = current.squares // Contains final values for 9 cells for current state.
 
+        const winner = this.calculateWinner(squares);
+
+        if(winner || squares[i]){
+            return;
+        }
+
         squares[i] = this.state.xIsNext ? 'X' : 'O';
 
         this.setState({
@@ -31,20 +37,55 @@ class Game extends React.Component{
 
     }
 
+    calculateWinner = (squares) => {
+        const possibilities = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for(let i = 0; i < possibilities.length; i++) {
+            const [a, b, c] = possibilities[i];
+            if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+              return squares[a];
+            }
+        }
+        return null;
+    }
+
     render(){
 
         const history = this.state.history;
         const current = history[this.state.stepNumber];
 
+        let status;
+        const winner = this.calculateWinner(current.squares);
+
+        if(winner){
+            status = 'Winner is ' + winner;
+        }else{
+            status = 'Next Player is ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div className="game">
                 <div className="game-board">
                     <Board clickAction = {(i) => this.handleClick(i)} squares = {current.squares} />
                 </div>
+
+                <div className="game-info">
+                    <div>
+                        {status}
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
-export default Game
+export default Game;
