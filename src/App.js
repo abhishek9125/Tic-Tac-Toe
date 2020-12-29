825ee2e61cbb5e4176e7c461ee0a84c75ec1c3b2
abhishek9125 <61241956+abhishek9125@users.noreply.github.com>
2020-12-29 21:03:03 +0530

    UI Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Game from './components/Game';
+
+class App extends React.Component{
+  render(){
+    return(
+      <Game/>
+    )
+  }
+}
 
 // Class Based Component Method -> Statefull
 // class App extends React.Component{
@@ -12,25 +21,25 @@ import React from 'react';
 // }
 
 // Functional Based Component Method -> Stateless
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
